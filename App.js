50ca6b2b64ce9c138081006310202c862124a44e
unknown <zachakraemer@gmail.com>
2021-07-15 11:45:34 -0400

    Changed style of App.js, added SelectLang.js and Game.js

--- App.js
+++ App.js
@@ -1,28 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import SelectLang from './components/SelectLang';
+import Game from './components/Game';
 
 export default function App() {
   const [alphIsPicked, setAlphIsPicked] = useState(false);
 	const [alphabet, setAlphabet] = useState(""); 
 
   return (
-    <View className='app'>
+    <View style={styles.container}>
     {alphIsPicked ? (
       <View>
-        <Text>Play Game</Text>
-        {/* <Game
+        <Game
           alphabet={alphabet}
           setAlphIsPicked={setAlphIsPicked}
-        /> */}
+        />
       </View>
     ) : (
       <View>
-        <Text>Select Alphabet</Text>
-        {/* <SelectLang
+        <SelectLang
           setAlphabet={setAlphabet}
           setAlphIsPicked={setAlphIsPicked}
-        /> */}
+        />
       </View>
     )}
     <StatusBar style="auto" />
@@ -33,8 +33,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#252d4a',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  selecterButton: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    marginVertical: 2,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#30A6E7',
+    color: 'white',
+    width: '100%',
   },
 });
